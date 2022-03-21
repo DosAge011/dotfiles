d88cec639dc5cb1f7d0a680ccf4ad6692d1889cc
DosAge011 <mynamewasthieved@gmail.com>
2022-03-20 17:08:47 -0700

    added picom config

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 nvim +PluginInstall +qall
 ln -s nvim ~/.config/nvim
 
+
+########## PICOM SETUP ##################
+
+ln -s $(pwd)/picom/picom.conf ~/.config/qtile/scripts/picom.conf
+
