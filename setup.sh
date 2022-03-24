e1072bc5763948ff4bacdb05a7f8a656bc6c748f
mike <mynamewasthieved@gmail.com>
2022-03-24 13:20:16 -0700

    add keybinds and updates to zsh and setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ GREEN='\033[0;32m'
 ZSH="$HOME/.zshrc"
 NVIM="$HOME/.config/nvim"
 PICOM="$HOME/.config/qtile/scripts/picom.conf"
+SXHKD="$HOME/.config/qtile/sxhkd/sxhkdrc"
 
 
 create_symlinks(){
@@ -34,6 +35,8 @@ sudo pacman -S starship
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 create_symlinks "ZSH" $(pwd)/zsh/.zshrc $ZSH
 
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 
 ########## NEOVIM SETUP ##################
@@ -46,6 +49,17 @@ nvim +PluginInstall +qall
 ########## PICOM SETUP ##################
 create_symlinks "PICOM" $(pwd)/picom/picom.conf $PICOM
 
+########## SXHKD SETUP ##################
+create_symlinks "SXHKD" $(pwd)/sxhkd/sxhkdrc $SXHKD
+
+############# ROFI ####################
+# mkdir temp
+# cd temp
+# git clone --depth=1 https://github.com/adi1090x/rofi.git
+# cd rofi
+# chmod +x setup.sh
+# ./setup.sh
+### create_symlinks "SXHKD" $(pwd)/sxhkd/sxhkdrc $SXHKD
 
 
 
